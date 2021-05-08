7971cbe2d04225bf93e28a3a934dc089e543fd49
ashiquet0858 <ashiquet0858@gmail.com>
2021-05-08 19:04:01 +0530

    upload12

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: "0.0.0.0",
-  url: env("https://manziladmin.herokuapp.com/"),
+  url: env("https://manziladmin.herokuapp.com"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
