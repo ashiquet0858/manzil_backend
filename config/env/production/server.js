aae58028e663f6c1f06cb9af6044ef45531102d0
ashiquet0858 <ashiquet0858@gmail.com>
2021-05-08 20:07:46 +0530

    upload13

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -7,3 +7,17 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: 'aws-region',
+      params: {
+        Bucket: 'my-bucket',
+      },
+    }, 
+  },
+});
